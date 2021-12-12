de687b08c1324ca520de342077fa7f8ba552215f
mashkust <mashkusti@gmail.com>
2021-12-12 18:18:29 +0300

    рендер

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import MessageView from './view/message-view.js';
 import PointListView from './view/point-list-view.js';
 import SiteMenuView from './view/site-menu-view.js';
 import SortView from './view/sort-view.js';
+import {render, RenderPosition, replace} from './render.js';
 import {generatePoint} from './mock/task.js';
-import {render, RenderPosition} from './render.js';
 
 const LENGTH_POINTS_ARRAY =15;
 
@@ -30,11 +30,11 @@ const renderTask = (taskListElement, point) => {
   const taskEditComponent = new FormEditView(point);
 
   const replaceCardToForm = () => {
-    taskListElement.replaceChild(taskEditComponent.element, taskComponent.element);
+    replace(taskEditComponent, taskComponent);
   };
 
   const replaceFormToCard = () => {
-    taskListElement.replaceChild(taskComponent.element, taskEditComponent.element);
+    replace(taskComponent, taskEditComponent);
   };
 
   const onEscKeyDown = (evt) => {
@@ -55,21 +55,16 @@ const renderTask = (taskListElement, point) => {
     document.removeEventListener('keydown', onEscKeyDown);
   });
 
-  // taskComponent.setEditClickHandler(() => {
-  //   replaceFormToCard();
-  //   document.removeEventListener('keydown', onEscKeyDown);
-  // });
-
-  render(taskListElement, taskComponent.element, RenderPosition.BEFOREEND);
+  render(taskListElement, taskComponent, RenderPosition.BEFOREEND);
 };
 
 if (POINTS.length === 0) {
-  render(siteEventsElement, new MessageView().element, RenderPosition.BEFOREEND);
+  render(siteEventsElement, new MessageView(), RenderPosition.BEFOREEND);
 }
 
-render(siteFiltersElement, new FilterView().element, RenderPosition.BEFOREEND);
-render(siteNavigationElement, new SiteMenuView().element, RenderPosition.BEFOREEND);
-render(siteEventsElement, new SortView().element, RenderPosition.AFTERBEGIN);
+render(siteFiltersElement, new FilterView(), RenderPosition.BEFOREEND);
+render(siteNavigationElement, new SiteMenuView(), RenderPosition.BEFOREEND);
+render(siteEventsElement, new SortView(), RenderPosition.AFTERBEGIN);
 
 for (let i=1; i<POINTS.length ;i++) {
   renderTask(siteEventsListElement, POINTS[i]);
