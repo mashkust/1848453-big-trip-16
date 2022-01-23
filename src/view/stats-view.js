3a416dc09c5d3d5c0b3fc1a200c9304506588779
mashkust <mashkusti@gmail.com>
2022-01-23 23:23:59 +0300

    так

---
+++ src/view/stats-view.js
@@ -0,0 +1,105 @@
+import Chart from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import SmartView from './smart-view.js';
+
+const moneyCtx = document.querySelector('#money');
+const typeCtx = document.querySelector('#type');
+const timeCtx = document.querySelector('#time');
+
+const BAR_HEIGHT = 55;
+
+const renderChart = (typeCtx, timeCtx, moneyCtx) => {
+  moneyCtx.height = BAR_HEIGHT * 5;
+  typeCtx.height = BAR_HEIGHT * 5;
+  timeCtx.height = BAR_HEIGHT * 5;
+
+  const moneyChart = new Chart(moneyCtx, {
+    plugins: [ChartDataLabels],
+    type: 'horizontalBar',
+    data: {
+      labels: ['TAXI', 'BUS', 'TRAIN', 'SHIP', 'FLIGHT', 'DRIVE'],
+      datasets: [{
+        data: [400, 300, 200, 160, 150, 100],
+        backgroundColor: '#ffffff',
+        hoverBackgroundColor: '#ffffff',
+        anchor: 'start',
+        barThickness: 44,
+        minBarLength: 50,
+      }],
+    },
+    options: {
+      responsive: false,
+      plugins: {
+        datalabels: {
+          font: {
+            size: 13,
+          },
+          color: '#000000',
+          anchor: 'end',
+          align: 'start',
+          formatter: (val) => `${val}x`,
+        },
+      },
+      title: {
+        display: true,
+        text: 'MONEY',
+        fontColor: '#000000',
+        fontSize: 23,
+        position: 'left',
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            fontColor: '#000000',
+            padding: 5,
+            fontSize: 13,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false,
+          },
+        }],
+        xAxes: [{
+          ticks: {
+            display: false,
+            beginAtZero: true,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false,
+          },
+        }],
+      },
+      legend: {
+        display: false,
+      },
+      tooltips: {
+        enabled: false,
+      },
+    },
+  });
+  return moneyChart;
+};
+
+const createStatsTemplate = () => (
+  ` <section class="statistics">
+  <h2 class="visually-hidden">Trip statistics</h2>
+
+  <div class="statistics__item">
+    <canvas class="statistics__chart" id="money" width="900"></canvas>
+  </div>
+
+  <div class="statistics__item">
+    <canvas class="statistics__chart" id="type" width="900"></canvas>
+  </div>
+
+  <div class="statistics__item">
+    <canvas class="statistics__chart" id="time" width="900"></canvas>
+  </div>
+</section>`);
+
+export default class SortsView extends SmartView {
+  get template() {
+    return createStatsTemplate();
+  }
+}
